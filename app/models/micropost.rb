d0aa6a3d2855796805616aa6640ee3410ee8f8c3
lflores1961 <fernando.flores1961@gmail.com>
2021-05-14 01:21:06 -0500

    Add Micropost generator presence validation

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,4 +3,5 @@ class Micropost < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 256 }
+  validates :generator, presence: true 
 end
